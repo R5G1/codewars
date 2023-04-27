08f4ef8cca9c83976d130c218d2127096e2ae78a
R5G1 <vlad.klimachevski@gmail.com>
2023-04-27 22:27:55 +0300

    feat: add index.html

--- js/8kyu.js
+++ js/8kyu.js
@@ -13,7 +13,8 @@ function doubleInteger(i) {
 
 function removeEveryOther(arr) {
     for (let i = 1; i < arr.length; i++) {
-      arr.splice(i, 1);
+        arr.splice(i, 1);
     }
     return arr;
-  }
+}
+
